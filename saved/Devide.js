0a6c72c1efdbdc5e5ed0be4fdbd8a1fff676bab8
iamshadmirza <md.shad1@yahoo.com>
2019-06-26 05:40:55 +0530

    combining animation

---
+++ saved/Devide.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Animated, TouchableWithoutFeedback } from "react-native";
+
+export default class animations extends Component {
+  
+  state = {
+    animation: new Animated.Value(0),
+  };
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 300,
+      duration: 1500,
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: 200,
+      }).start();
+    });
+  };
+  render() {
+    const randomValue = new Animated.Value(2);
+    const newAnimation = Animated.divide(this.state.animation, randomValue);
+    const animatedStyles={
+      transform: [{ translateY: newAnimation }],
+    }
+    return (
+      <View style={styles.container}>
+          <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animatedStyles]} />
+        </TouchableWithoutFeedback>
+      </View>
+    );
+
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'red'
+  }
+});
---
+++ saved/Parallel.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Animated, TouchableWithoutFeedback } from "react-native";
+
+export default class animations extends Component {
+  
+  state = {
+    colorAnimation: new Animated.Value(0),
+    scaleAnimation: new Animated.Value(0)
+  };
+  startAnimation = () => {
+    Animated.parallel([
+      Animated.timing(this.state.colorAnimation, {
+        toValue: 1,
+        duration: 500,
+      }),
+      Animated.timing(this.state.scaleAnimation, {
+        toValue: 2,
+        duration: 300
+      })
+    ]).start();
+  };
+
+  render() {
+    
+    const bgColorInterpolate = this.state.colorAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"]
+    });
+
+    const boxStyle = {
+      backgroundColor: bgColorInterpolate,
+      transform: [{ scale: this.state.scaleAnimation }],
+    };
+
+    return (
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, boxStyle]} />
+        </TouchableWithoutFeedback>
+      </View>
+    );
+
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'red'
+  }
+});
